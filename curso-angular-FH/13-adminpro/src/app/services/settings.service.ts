608de5c14ffd378a48ee1c6e906d900c8c81e82c
iara baya <iarabayav@gmail.com>
2022-01-12 12:24:06 -0300

    settings service finished with functionalities

--- curso-angular-FH/13-adminpro/src/app/services/settings.service.ts
+++ curso-angular-FH/13-adminpro/src/app/services/settings.service.ts
@@ -11,4 +11,31 @@ export class SettingsService {
     const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
     this.linkTheme?.setAttribute('href', url);
   }
+
+  changeTheme( theme: string ){
+    const url = `./assets/css/colors/${theme}.css`;
+    
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+    
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme(): void {
+    const links = document.querySelectorAll('.selector');
+    
+    links.forEach( el => {
+      el.classList.remove('working');
+
+      const btnTheme = el.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+      
+      if(currentTheme === btnThemeUrl){
+        el.classList.add('working');
+      }
+    })
+
+  }
+
 }
